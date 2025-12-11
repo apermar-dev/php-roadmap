423f4a908152c3fbb4a100c735af5030e6338bf1
apermar1301 <apermar1301@g.educaand.es>
2025-12-11 13:37:01 +0100

    Roadmap: Ejercicio 1 RESUELTO

---
+++ roadmap/00.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+* ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+* - Recuerda que todas las instrucciones de participación están en el
+*   repositorio de GitHub.
+*
+* Lo primero... ¿Ya has elegido un lenguaje?
+* - No todos son iguales, pero sus fundamentos suelen ser comunes.
+* - Este primer reto te servirá para familiarizarte con la forma de participar
+*   enviando tus propias soluciones.
+*
+* EJERCICIO:
+* - Crea un comentario en el código y coloca la URL del sitio web oficial del
+*   lenguaje de programación que has seleccionado.
+* - Representa las diferentes sintaxis que existen de crear comentarios
+*   en el lenguaje (en una línea, varias...).
+* - Crea una variable (y una constante si el lenguaje lo soporta).
+* - Crea variables representando todos los tipos de datos primitivos
+*   del lenguaje (cadenas de texto, enteros, booleanos...).
+* - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+*
+* ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+* debemos comenzar por el principio.
+*/
+
+
+// Comentarios
+
+// https://www.php.net/
+# https://www.php.net/
+
+/*
+* https://www.php.net/
+*/
+
+
+
+// Variables y Constantes
+
+$num = 1; # VARIABLE
+
+define("pi", 3.14); # CONSTANTE
+
+
+// Creación de variables con tipos de datos primitivos
+
+$boolean = true; # BOOLEAN
+$int = 1234; # ENTERO
+$float = 1.234; # FLOAT
+$string = "Hello World"; # STRING
+$string_num = "10.5"; # STRING NUMÉRICOS (SE INTERPRETAN COMO NÚMEROS) 
+$array = array("uno", "dos", "tres", false, 4);
+
+$php = "php";
+
+echo "¡Hola, $php!";
+
